f2d7f9480ee73250f6d4d91a0b149841efdd3fab
Camila Duran <camilarxn023@gmail.com>
2022-10-30 18:52:19 -0500

    Finalizacion colecciones trabajo dos python

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import list;
+import sets;
+import tuples;
+import dictionaries;
 
-print(list.estudiantes[3]);
+#LISTA
+# list.estudiantes.remove('Alex')
+# list.estudiantes.clear();
+# print(list.estudiantes);
+
+#SETS
+# sets.setProductos.remove('Hdmi');
+# sets.setProductos.discard('Ventilador');
+# print(sets.setProductos);
+
+#TUPLES
+# tuples.año.add('February');
+# tuples.año.copy();
+# print(tuples.año)
+
+#DICTIONARY
+#dictionaries.dicPerson1.values();
+# dictionaries.dicPerson1.get('Nombres');
+# print(dictionaries.dicPerson1)
